8082ab6c57dcf86a6b6f8c1f55724a96905b51ea
Pete Prodoehl <pete@P2-MacBook.local>
2016-03-14 12:58:27 -0500

    Version 0.13 of the SpaceAPI and addition of webcam

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ header('Cache-Control: no-cache');
 header('Content-type: application/json');
 
 $time	= time();
-$open	= 'false';
+$open	= 'null';
 $status	= 'members only';  
 $day	= date("N", $time);
 $hour 	= date("G", $time);
@@ -14,40 +14,63 @@ $hour 	= date("G", $time);
 if (($day == 2) or ($day == 4)) {
 	// check for 7pm to 9pm)
 	if (($hour > 18) and ($hour < 21)) {
-		$open = 'true';
+		$open = '"true"';
 		$status = 'open for public';
 		
 	}
 	else {
-		$open = 'false';
+		$open = 'null';
 		$status = 'members only';
 	}
 }
 ?>
 {
-  "api":"0.12",
+  "api":"0.13",
   "space":"Milwaukee Makerspace",
   "url":"http:\/\/milwaukeemakerspace.org",
-  "address":"3073 S Chase Ave, Bldg 34, Milwaukee, WI 53207, USA",
-  "lat":42.988773,
-  "lon":-87.912791,
+  "location":{
+    "address":"2555 South Lenox Street, Milwaukee, WI 53207, USA",
+    "lat":42.998085,
+    "lon":-87.898484
+  },
   "logo":"http:\/\/apps.2xlnetworks.net\/milwaukeemakerspace\/images\/milwaukeemakerspace.png",
-  "icon":{
-    "open":"http:\/\/apps.2xlnetworks.net\/milwaukeemakerspace\/images\/open.png",
-    "closed":"http:\/\/apps.2xlnetworks.net\/milwaukeemakerspace\/images\/closed.png"
-  },
   "contact":{
+    "email":"info@milwaukeemakerspace.org",
     "twitter":"@mkemakerspace",
-    "ml":"milwaukeemakerspace@googlegroups.com"
+    "ml":"milwaukeemakerspace@googlegroups.com",
+    "issue_mail":"pete.prodoehl@milwaukeemakerspace.org"
   },
-  "open":<?php echo $open ?>,
-  "status":"<?php echo $status ?>",
-  "feeds":[
-    {"name":"blog","type":"application/rss+xml","url":"http://milwaukeemakerspace.org/feed/"},
-    {"name":"wiki","type":"application/rss+xml","url":"http://wiki.milwaukeemakerspace.org/feed.php"},
-    {"name":"photos","type":"text/xml","url":"http://api.flickr.com/services/feeds/groups_pool.gne?id=1554189@N20&lang=en-us&format=rss_200"},
-    {"name":"calendar","type":"text/calendar","url":"http://www.google.com/calendar/ical/milwaukeemakerspace.org_kckatpfskv1lerggn73utbadgc%40group.calendar.google.com/public/basic.ics"}
+  "issue_report_channels": [
+    "issue_mail"
   ],
+  "cam": [
+	"http://apps.2xlnetworks.net/milwaukeemakerspace/webcam/"
+  ],
+  "state":{
+    "open":<?php echo $open ?>,
+    "icon":{
+      "open":"http:\/\/apps.2xlnetworks.net\/milwaukeemakerspace\/images\/open.png",
+      "closed":"http:\/\/apps.2xlnetworks.net\/milwaukeemakerspace\/images\/closed.png"
+    }
+  },
+    "feeds": {
+        "blog": {
+            "type": "application/rss+xml",
+            "url": "http://milwaukeemakerspace.org/feed/"
+        },
+        "wiki": {
+            "type": "application/rss+xml",
+            "url": "http://wiki.milwaukeemakerspace.org/feed.php"
+        },
+        "calendar": {
+            "type": "ical",
+            "url": "http://www.google.com/calendar/ical/milwaukeemakerspace.org_kckatpfskv1lerggn73utbadgc%40group.calendar.google.com/public/basic.ics"
+        },
+        "photos": {
+            "type": "flickr",
+            "url": "http://api.flickr.com/services/feeds/groups_pool.gne?id=1554189@N20&lang=en-us&format=rss_200"
+        }
+    },
   "lastchange":<?php echo $time ?>
   
 }
